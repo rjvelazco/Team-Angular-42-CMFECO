764d204c465efc0f2dab97d05fa503cff0ac311c
rjvelazco <rjvelazco21@gmail.com>
2021-02-11 09:06:55 -0400

    Header Component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common'
+
+// Components
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
-  declarations: [],
-  exports: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class SharedModule {
