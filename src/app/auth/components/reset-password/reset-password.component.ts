8010162da1e0e1c034387a7e5510213ebe402111
rjvelazco <rjvelazco21@gmail.com>
2021-02-17 22:00:26 -0400

    fix: issue #16 - button login added & header service created

--- src/app/auth/components/reset-password/reset-password.component.ts
+++ src/app/auth/components/reset-password/reset-password.component.ts
@@ -1,28 +1,36 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 import Swal from 'sweetalert2';
 
 // Services
 import {AuthService} from 'src/app/core/services/auth.service';
+import { HeaderService } from '../../../core/services/header.service';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.css']
 })
-export class ResetPasswordComponent implements OnInit {
+export class ResetPasswordComponent implements OnInit, OnDestroy {
 
   public form: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
+    private headerService: HeaderService,
   ) {
   }
 
   ngOnInit(): void {
     this.buildForm();
+    this.headerService.showLoginBtn.emit(true);
+  }
+
+  ngOnDestroy(): void {
+    this.headerService.showLoginBtn.emit(false);
+    // this.headerService.showLoginBtn = false;
   }
 
   get emailInvalid() {
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// services
+import { HeaderService } from '../../../core/services/header.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public showBackLoginBtn: boolean = false; 
+
+  constructor(
+    private headerService: HeaderService
+  ) { }
 
   ngOnInit(): void {
+    this.headerService.showLoginBtn.subscribe(data => {
+      this.showBackLoginBtn = data;
+    })
   }
 
 }
