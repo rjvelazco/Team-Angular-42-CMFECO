3373a00f470484b64eb7bb6c650d1cc04cdbe8d7
Santiago Gomez Florez <santiagofl123@gmail.com>
2021-02-19 12:24:26 -0500

    Estructura de home-page

---
+++ src/app/home-page/components/side-element/side-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-element',
+  templateUrl: './side-element.component.html',
+  styleUrls: ['./side-element.component.css']
+})
+export class SideElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CarouselComponent } from './carousel/carousel.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClockComponent } from './components/clock/clock.component';
+import { SideElementComponent } from './components/side-element/side-element.component';
 
 
 
 @NgModule({
-  declarations: [CarouselComponent],
+  declarations: [
+    CarouselComponent,
+    DashboardComponent,
+    ClockComponent,
+    SideElementComponent,
+  ],
   imports: [
     CommonModule
   ]
