8c00226f5dc5f29bbba4480032faeddb44aa251b
Santiago Gomez Florez <santiagofl123@gmail.com>
2021-03-09 20:10:32 -0500

    Perfil y eventos

--- src/app/pages/profile/components/my-profile/my-profile.component.ts
+++ src/app/pages/profile/components/my-profile/my-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-my-profile',
@@ -7,7 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyProfileComponent implements OnInit {
 
-  constructor() { }
+  myEvents = [
+    {
+      'id': 1,
+      'image': 'banner_event.png',
+      'event': 'Community Fest and Code',
+      'description': 'Evento nuevo y innovador donde podran mejorar sus habilidades y conocer a nuevos programadores de todas las tecnologias',
+      'information': '',
+    },
+    {
+      'id': 2,
+      'image': 'banner_event.png',
+      'event': 'Community Fest and Code',
+      'description': 'Evento nuevo y innovador donde podran mejorar sus habilidades y conocer a nuevos programadores de todas las tecnologias',
+      'information': '',
+    },
+    {
+      'id': 3,
+      'image': 'banner_event.png',
+      'event': 'Community Fest and Code',
+      'description': 'Evento nuevo y innovador donde podran mejorar sus habilidades y conocer a nuevos programadores de todas las tecnologias',
+      'information': '',
+    },
+    {
+      'id': 4,
+      'image': 'banner_event.png',
+      'event': 'Community Fest and Code',
+      'description': 'Evento nuevo y innovador donde podran mejorar sus habilidades y conocer a nuevos programadores de todas las tecnologias',
+      'information': '',
+    },
+    {
+      'id': 5,
+      'image': 'banner_event.png',
+      'event': 'Community Fest and Code',
+      'description': 'Evento nuevo y innovador donde podran mejorar sus habilidades y conocer a nuevos programadores de todas las tecnologias',
+      'information': '',
+    },
+    {
+      'id': 6,
+      'image': 'banner_event.png',
+      'event': 'Community Fest and Code',
+      'description': 'Evento nuevo y innovador donde podran mejorar sus habilidades y conocer a nuevos programadores de todas las tecnologias',
+      'information': '',
+    }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
