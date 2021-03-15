e0d68b37e08fb3ad8cface53ba0372b448572320
Santiago Gomez Florez <santiagofl123@gmail.com>
2021-03-14 21:47:17 -0500

    new insignias

--- src/app/pages/profile/components/insignias/insignias.component.ts
+++ src/app/pages/profile/components/insignias/insignias.component.ts
@@ -10,25 +10,25 @@ export class InsigniasComponent implements OnInit {
   dumpData = [
     {
       "insignia": "King of Code",
-      "img": '../../../../assets/img/insignias/corona.png',
+      "img": 'corona.png',
       "detalle": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam beatae commodi culpa, cum, dignissimos dolorem.",
       "pasos": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam beatae commodi culpa, cum, dignissimos dolorem eligendi facilis ipsam iure maxime, natus necessitatibus nesciunt nihil nobis quae quam quas quisquam."
     },
     {
       "insignia": "Hacker",
-      "img": '../../../../assets/img/insignias/hacker.jpg',
+      "img": 'hacker.png',
       "detalle": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam beatae commodi culpa, cum, dignissimos dolorem.",
       "pasos": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam beatae commodi culpa, cum, dignissimos dolorem eligendi facilis ipsam iure maxime, natus necessitatibus nesciunt nihil nobis quae quam quas quisquam."
     },
     {
       "insignia": "Master Code",
-      "img": '../../../../assets/img/insignias/masterCode.png',
+      "img": 'masterCode.png',
       "detalle": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam beatae commodi culpa, cum, dignissimos dolorem.",
       "pasos": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam beatae commodi culpa, cum, dignissimos dolorem eligendi facilis ipsam iure maxime, natus necessitatibus nesciunt nihil nobis quae quam quas quisquam."
     },
     {
       "insignia": "Sociable",
-      "img": '../../../../assets/img/insignias/sociable.jpg',
+      "img": 'sociable.png',
       "detalle": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam beatae commodi culpa, cum, dignissimos dolorem.",
       "pasos": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam beatae commodi culpa, cum, dignissimos dolorem eligendi facilis ipsam iure maxime, natus necessitatibus nesciunt nihil nobis quae quam quas quisquam."
     },
