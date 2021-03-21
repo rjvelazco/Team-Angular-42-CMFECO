d28836780efa45ed317e492414b9cebf3f19dc0b
rjvelazco <rjvelazco21@gmail.com>
2021-03-21 00:19:05 -0400

    Team Angular 42 CMFECO - Project

--- src/app/pages/profile/components/insignias/insignias.component.ts
+++ src/app/pages/profile/components/insignias/insignias.component.ts
@@ -1,20 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {InsigniasService} from 'src/app/core/services/insignias.service';
 import {UsuarioService} from 'src/app/core/services/usuario.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-insignias',
   templateUrl: './insignias.component.html',
   styleUrls: ['./insignias.component.css']
 })
-export class InsigniasComponent implements OnInit {
-  insigniasUsuario = [];
-  insigniasGanadas = [];
+export class InsigniasComponent implements OnInit, OnDestroy {
+
+  public insigniasUsuario = [];
+  public insigniasGanadas = [];
+
+  public subcription: Subscription;
 
   constructor(private insigniasService: InsigniasService, private usuarioService: UsuarioService) { }
 
   ngOnInit() {
     this.mostrarInsignias();
+    this.subcription = this.usuarioService.usuarioEmiter.subscribe(user => this.mostrarInsignias());
+  }
+
+  ngOnDestroy() {
+    this.subcription.unsubscribe();
   }
 
   mostrarInsignias(){
