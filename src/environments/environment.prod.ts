fa56e3158d3a6c3c780094c4d77f861b475d20d8
rjvelazco <rjvelazco21@gmail.com>
2021-03-20 23:03:39 -0400

    Enviroment - Production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCNF-Db3bMeaM1vzVBxXEgRKaMESsf6Vqw",
+    authDomain: "community-fest-code.firebaseapp.com",
+    projectId: "community-fest-code",
+    storageBucket: "community-fest-code.appspot.com",
+    messagingSenderId: "49040123079",
+    appId: "1:49040123079:web:337dc7f8b35c59bde94ae2"
+  }
 };
